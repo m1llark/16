ebf7ec8d0892b50e1dd1cc5393a7ac981d5fcf59
m1llark <ponomarevm539@gmail.com>
2023-04-01 02:43:08 +0500

    commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -5,18 +5,11 @@ public class Main {
     public static void main(String[] args) {
         UserServiceImpl userService = new UserServiceImpl();
         userService.createUsersTable();
-        userService.saveUser("John", "Watson", (byte) 45);
         userService.saveUser("Joe", "Moe", (byte) 23);
         userService.saveUser("Tom", "Jerriev", (byte) 22);
-        userService.saveUser("lous", "Mous", (byte) 17);
-        userService.cleanUsersTable();
         userService.saveUser("Cristiano", "Ronaldo", (byte) 37);
         userService.saveUser("a", "Jerriev", (byte) 22);
-        userService.saveUser("b", "Mous", (byte) 17);
-        userService.getAllUsers();
-        userService.removeUserById(3);
-        userService.getAllUsers();
-
+        userService.cleanUsersTable();
         userService.dropUsersTable();
    }
 }
